8024b6387afb3b9c04c9bb8608239a0f9fe6b2f8
Balazs <hevele.balazs@gmail.com>
2016-12-29 16:19:12 +0200

    moving circle - line collision

--- collision.js
+++ collision.js
@@ -89,8 +89,101 @@ function update() {
 	timer = setTimeout(update, tick);
 }
 
+function distance(x1, y1, x2, y2) {
+	var dx = x1 - x2;
+	var dy = y1 - y2;
+	return Math.sqrt(dx * dx + dy * dy);
+}
+
+/* does (x2, y2) -> (x3, y3) turn right to (x1, y1) -> (x2, y2)? */
+function direction(x1, y1, x2, y2, x3, y3) {
+    var dx1 = x2 - x1;
+    var dy1 = y2 - y1;
+    var dx2 = x3 - x2;
+    var dy2 = y3 - y2;
+    /* calculate
+        | dx1 dx2 | 
+        | dy1 dy2 |
+    */
+    var det = dx1 * dy2 - dx2 * dy1;
+    return (det > 0);
+}
+
+/* does the static circle touch the line? */
+function circleline(circle, line) {
+	/* point distance */
+	var dist1 = distance(circle.x, circle.y, line.x1, line.y1);
+	var dist2 = distance(circle.x, circle.y, line.x2, line.y2);
+	
+	if (dist1 < circle.r || dist2 < circle.r) return true;
+	
+	/* side collision */
+    var distx = line.x1 - line.x2;
+    var disty = line.y1 - line.y2;
+    var angle = Math.atan2(disty, distx);
+    /* point 1 */
+    var angle1 = angle + Math.PI / 2;
+    var x1 = circle.x + circle.r * Math.cos(angle1);
+    var y1 = circle.y + circle.r * Math.sin(angle1);
+    /* point 2 */
+    var angle2 = angle - Math.PI / 2;
+    var x2 = circle.x + circle.r * Math.cos(angle2);
+    var y2 = circle.y + circle.r * Math.sin(angle2);
+    
+    var dir1 = direction(x1, y1, x2, y2, line.x1, line.y1);
+    var dir2 = direction(x1, y1, x2, y2, line.x2, line.y2);
+    
+    var dir3 = direction(x1, y1, line.x1, line.y1, line.x2, line.y2);
+    var dir4 = direction(x2, y2, line.x1, line.y1, line.x2, line.y2);
+    
+    if (dir1 != dir2 && dir3 != dir4) return true;
+    
+    return false;
+}
+
+/* does line (x11, y11)->(x12, y12) cross line (x21, y21)->(x22, y22)? */
+function linecross(x11, y11, x12, y12, x21, y21, x22, y22) {
+	var dir1 = direction(x11, y11, x21, y21, x22, y22);
+	var dir2 = direction(x12, y12, x21, y21, x22, y22);
+	if (dir1 == dir2) return false;
+	
+	var dir3 = direction(x21, y21, x11, y11, x12, y12);
+	var dir4 = direction(x22, y22, x11, y11, x12, y12);
+	if (dir3 == dir4) return false;
+	
+	return true;
+}
+
+/* is point (x, y) inside rectangle (x1, y1) (x2, y2) (x3, y3) (x4, y4)? */
+function inrect(x, y, x1, y1, x2, y2, x3, y3, x4, y4) {
+	var dir1 = direction(x, y, x1, y1, x2, y2);
+	var dir2 = direction(x, y, x2, y2, x3, y3);
+	var dir3 = direction(x, y, x3, y3, x4, y4);
+	var dir4 = direction(x, y, x4, y4, x1, y1);
+	
+	return (dir1 == dir2) && (dir2 == dir3) && (dir3 == dir4);
+}
+
 /* does the moving circle touch the line? */
-function linetouch(circle, line) {
+function mcircleline(circle, line) {
+	/* do end circles touch the line? */
+	var touch1 = circleline({x: circle.x,   y: circle.y,   r: circle.r}, line);
+	var touch2 = circleline({x: circle.tox, y: circle.toy, r: circle.r}, line);
+	
+	if (touch1 || touch2) return true;
+	
+	/* does line cross any helper line? */
+	var cross1 = linecross(line.x1, line.y1, line.x2, line.y2, circle.x1, circle.y1, circle.tox1, circle.toy1);
+	var cross2 = linecross(line.x1, line.y1, line.x2, line.y2, circle.x2, circle.y2, circle.tox2, circle.toy2);
+	
+	if (cross1 || cross2) return true;
+	
+	/* is any line point inside helper rectangle? */
+	var in1 = inrect(line.x1, line.y1, circle.x1, circle.y1, circle.tox1, circle.toy1, circle.tox2, circle.toy2, circle.x2, circle.y2);
+	var in2 = inrect(line.x2, line.y2, circle.x1, circle.y1, circle.tox1, circle.toy1, circle.tox2, circle.toy2, circle.x2, circle.y2);
+	
+	if (in1 || in2) return true;
+	
 	return false;
 }
 
@@ -134,7 +227,7 @@ function mousemove(event) {
 	var i;
 	for (i = 0; i < lines.length; ++i) {
 		var line = lines[i];
-		lines[i].touch = linetouch(circle, line);
+		lines[i].touch = mcircleline(circle, line);
 	}
 }
 
